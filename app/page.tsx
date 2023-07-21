f5081b7631ebb5698ed8934726f53a28185ddc42
akkshitgupta <adamantaksh18@gmail.com>
2023-07-21 14:27:01 +0530

    nextjs api routes initiated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Card from "@components/Card";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      {/* card container */}
+      <section className="mx-auto h-96 w-2/3 rounded-md border-2 border-green-600 p-4 ">
+        <Card title={"hello"} />
+      </section>
+    </main>
   );
 }
